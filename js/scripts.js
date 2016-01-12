43fa9775d7bbdbdf2a8e755a3c50fd8a5a483d6b
eddieduro <student@epicodus.com>
2016-01-12 10:13:05 -0800

    js to make inputs show up in story

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput = $("input#animal").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
+
+    $("#story").css("visibility", "visible");
+    event.preventDefault();
+  });
+});
