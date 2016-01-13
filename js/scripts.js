f5f26df5c9d09bb1906f01fb13228aa2bee88747
eddie duro and jared beckler <jared.beckler@gmail.com>
2016-01-13 15:19:13 -0800

    refactor js into loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,13 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
-
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-
-    $("#story").css("visibility", "visible");
+    var blanks = ['person1', 'person2', 'animal', 'verb', 'noun'];
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      console.log(userInput);
+      $("." + blank).text(userInput);
+    });
+    
+  $("#story").css("visibility", "visible");
     event.preventDefault();
   });
 });
